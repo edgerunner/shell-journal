7ccc4fa81874d6d75b4d0786db676a03165f5f4b
Mert Torun <mert@merttorun.com>
2021-02-01 18:49:43 +0300

    show list in 4 columns

--- src/Runner/List.elm
+++ src/Runner/List.elm
@@ -16,8 +16,22 @@ step1 : Runner
 step1 =
     Runner.run
         |> Runner.handleGotPageList
-            (String.join "\n"
+            (clump
+                >> String.join "\n"
                 >> String.replace ".shjo" ""
+                >> String.replace "-w" " week "
+                >> String.replace "-q" " quarter "
+                >> String.cons '\n'
                 >> Runner.log
                 >> (|>) Runner.done
             )
+
+
+clump : List String -> List String
+clump list =
+    case list of
+        s1 :: s2 :: s3 :: s4 :: rest ->
+            ([ s1, s2, s3, s4 ] |> String.join " \t") :: clump rest
+
+        other ->
+            other |> String.join " \t" |> List.singleton
